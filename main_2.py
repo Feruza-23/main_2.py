cf1c1d4291a900491e1ad323ca0013939a67e3e3
Feruza-23 <feruza.abdukadyrova.23@mail.ru>
2024-06-16 18:14:08 +0400

    Create main_2.py

---
+++ main_2.py
@@ -0,0 +1,21 @@
+import random 
+
+all_symbols = 'QWERTYIOPASDFGHJKLZXCVBNMqwertyiopasdfghjklzxcvbnm!?/_.@#-0123456789'
+
+lenght = int(input('Введите кол-во символов для енерации пароля: '))
+
+password = ''
+
+
+sogl = 'qwrtpsdghjklxcvbnm'
+glas = 'eyuioa'
+numbers = '01234567890'
+symbol = '@#%?!'
+password = ""
+for i in range(4):
+    password += random.choice(sogl)
+    password += random.choice(glas)
+for i in range(2):
+    password += random.choice(numbers)
+password += random.choice(symbol)
+print(password)
